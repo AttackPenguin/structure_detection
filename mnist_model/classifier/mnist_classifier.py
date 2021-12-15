8298beb9aeed4e1c80ff9940c8c4711c4cc445be
AttackPenguin <denis.e.hudon@gmail.com>
2021-12-15 13:47:41 -0500

    Ready to generate classifier

--- mnist_model/classifier/mnist_classifier.py
+++ mnist_model/classifier/mnist_classifier.py
@@ -21,7 +21,7 @@ class_names = list(range(10))
 
 model = models.Sequential()
 model.add(layers.Conv2D(128, (3, 3), activation='relu',
-                        input_shape=(28, 28, 1)))
+                        input_shape=(28, 28)))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
