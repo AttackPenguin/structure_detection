e59280319911b5b8b70552dd01ca8a1149e6b26e
AttackPenguin <denis.e.hudon@gmail.com>
2021-12-15 08:02:17 -0500

    file save issues?

--- mnist_model/autoencoder_v1_testing.py
+++ mnist_model/autoencoder_v1_testing.py
@@ -93,16 +93,16 @@ def build_and_store_models():
 
     """
 
-    (x_train, _), (x_test, _) = mnist.load_data()
-
-    x_train = x_train.astype('float32') / 255.
+    # (x_train, _), (x_test, _) = mnist.load_data()
+    #
+    # x_train = x_train.astype('float32') / 255.
 
     for n in range(1, 21):
 
-        np.random.shuffle(x_train)
-        np.random.shuffle(x_test)
-        x_data = x_train[:50_000]
-        v_data = x_train[50_000:]
+        # np.random.shuffle(x_train)
+        # np.random.shuffle(x_test)
+        # x_data = x_train[:50_000]
+        # v_data = x_train[50_000:]
 
         autoencoder = Autoencoder(
             outer_layer=100,
@@ -113,21 +113,21 @@ def build_and_store_models():
         autoencoder.compile(optimizer='adam',
                             loss=losses.MeanSquaredError())
 
-        dttm_fit_start = pd.Timestamp.now()
-        print(f"Started Fitting Encoder, x={n}...")
+        # dttm_fit_start = pd.Timestamp.now()
+        # print(f"Started Fitting Encoder, x={n}, time={dttm_fit_start}...")
+        #
+        # callback = EarlyStopping(monitor='val_loss', patience=10,
+        #                          restore_best_weights=True,
+        #                          min_delta=0.0001)
+        #
+        # autoencoder.fit(x_data, x_data,
+        #                 epochs=1000, callbacks=[callback],
+        #                 shuffle=False,
+        #                 validation_data=(v_data, v_data))
 
-        callback = EarlyStopping(monitor='val_loss', patience=10,
-                                 restore_best_weights=True,
-                                 min_delta=0.0001)
-
-        autoencoder.fit(x_data, x_data,
-                        epochs=1000, callbacks=[callback],
-                        shuffle=False,
-                        validation_data=(v_data, v_data))
-
-        save_file = os.path.join(model_dir, f"v1_n_{n}")
+        save_file = os.path.join(model_dir, f"v1_n_{n}.pickle")
         with open(save_file, 'wb') as file:
-            pass
+            pickle.dump(autoencoder, file)
 
 
 if __name__ == '__main__':
