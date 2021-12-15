acfce4cfb7443888718812c9fa4d5df7aa41c7bc
AttackPenguin <denis.e.hudon@gmail.com>
2021-12-15 08:15:51 -0500

    file save issues?

--- mnist_model/autoencoder_v1_testing.py
+++ mnist_model/autoencoder_v1_testing.py
@@ -44,7 +44,7 @@ def get_mse_results():
     data = {x: list() for x in range(1, 21)}
 
     for _ in range(10):
-        for x in range(1, 21):
+        for n in range(1, 21):
 
             np.random.shuffle(x_train)
             np.random.shuffle(x_test)
@@ -54,13 +54,13 @@ def get_mse_results():
             autoencoder = Autoencoder(
                 outer_layer=100,
                 inner_layer=100,
-                latent_dim=x
+                latent_dim=n
             )
 
             autoencoder.compile(optimizer='adam', loss=losses.MeanSquaredError())
 
             dttm_fit_start = pd.Timestamp.now()
-            print(f"Started Fitting Encoder, x={x}...")
+            print(f"Started Fitting Encoder, x={n}...")
 
             callback = EarlyStopping(monitor='val_loss', patience=10,
                                      restore_best_weights=True,
@@ -71,14 +71,10 @@ def get_mse_results():
                             shuffle=False,
                             validation_data=(v_data, v_data))
 
-            # autoencoder.save_weights(f'./models/dim_10.h5')
-            # autoencoder.build((1, 28, 28))
-            # autoencoder.load_weights('./models/scratch_vsmall.h5')
-
             results = list()
             for i in x_test[:1000]:
                 results.append(compare(autoencoder, i[np.newaxis, :, :]))
-            data[x].append(np.mean(results))
+            data[n].append(np.mean(results))
             print(np.mean(results))
 
     with open('./data/latent_dim_of_10.pickle', 'wb') as file:
@@ -93,16 +89,16 @@ def build_and_store_models():
 
     """
 
-    # (x_train, _), (x_test, _) = mnist.load_data()
-    #
-    # x_train = x_train.astype('float32') / 255.
+    (x_train, _), (x_test, _) = mnist.load_data()
+
+    x_train = x_train.astype('float32') / 255.
 
     for n in range(1, 21):
 
-        # np.random.shuffle(x_train)
-        # np.random.shuffle(x_test)
-        # x_data = x_train[:50_000]
-        # v_data = x_train[50_000:]
+        np.random.shuffle(x_train)
+        np.random.shuffle(x_test)
+        x_data = x_train[:50_000]
+        v_data = x_train[50_000:]
 
         autoencoder = Autoencoder(
             outer_layer=100,
@@ -113,21 +109,20 @@ def build_and_store_models():
         autoencoder.compile(optimizer='adam',
                             loss=losses.MeanSquaredError())
 
-        # dttm_fit_start = pd.Timestamp.now()
-        # print(f"Started Fitting Encoder, x={n}, time={dttm_fit_start}...")
-        #
-        # callback = EarlyStopping(monitor='val_loss', patience=10,
-        #                          restore_best_weights=True,
-        #                          min_delta=0.0001)
-        #
-        # autoencoder.fit(x_data, x_data,
-        #                 epochs=1000, callbacks=[callback],
-        #                 shuffle=False,
-        #                 validation_data=(v_data, v_data))
+        dttm_fit_start = pd.Timestamp.now()
+        print(f"Started Fitting Encoder, n={n}, time={dttm_fit_start}...")
 
-        save_file = os.path.join(model_dir, f"v1_n_{n}.pickle")
-        with open(save_file, 'wb') as file:
-            pickle.dump(autoencoder, file)
+        callback = EarlyStopping(monitor='val_loss', patience=10,
+                                 restore_best_weights=True,
+                                 min_delta=0.0001)
+
+        autoencoder.fit(x_data, x_data,
+                        epochs=1000, callbacks=[callback],
+                        shuffle=False,
+                        validation_data=(v_data, v_data))
+
+        save_file = os.path.join(model_dir, f"v1_n_{n}")
+        autoencoder.save(save_file)
 
 
 if __name__ == '__main__':
