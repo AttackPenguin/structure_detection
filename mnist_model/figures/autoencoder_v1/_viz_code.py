92b2a34f5972884c41aec3cc1fe24e9bed509dd2
AttackPenguin <denis.e.hudon@gmail.com>
2021-12-16 12:47:47 -0500

    autoencoder_v1 shown to perform poorly in results.

--- mnist_model/figures/autoencoder_v1/_viz_code.py
+++ mnist_model/figures/autoencoder_v1/_viz_code.py
@@ -2,23 +2,28 @@ import os
 import pickle
 
 import numpy as np
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, gridspec
 from matplotlib.ticker import MaxNLocator
+from sklearn.decomposition import PCA
 import seaborn as sns
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import models
 
 sns.set_theme(context='paper')
 
 fig_dir = 'mnist_model/figures/autoencoder_v1'
 data_dir = 'mnist_model/data'
+model_dir = 'mnist_model/models'
 
 
 def main():
-    plot_latent_dim_of_10_means()
-    plot_latent_dim_of_10_boxplots()
+    # plot_latent_dim_of_10_means()
+    # plot_latent_dim_of_10_boxplots()
+    # visualize_transformed_images()
+    pca_visualization()
 
 
 def plot_latent_dim_of_10_means():
-
     data_file = os.path.join(data_dir, 'latent_dim_of_10.pickle')
     with open(data_file, 'rb') as file:
         data = pickle.load(file)
@@ -42,7 +47,6 @@ def plot_latent_dim_of_10_means():
 
 
 def plot_latent_dim_of_10_boxplots():
-
     data_file = os.path.join(data_dir, 'latent_dim_of_10.pickle')
     with open(data_file, 'rb') as file:
         data = pickle.load(file)
@@ -63,6 +67,83 @@ def plot_latent_dim_of_10_boxplots():
     fig.show()
 
     save_file = os.path.join(fig_dir, '01b_autoencoder_v1_boxplots_mse.png')
+    fig.savefig(save_file)
+
+
+def visualize_transformed_images():
+    (_, _), (x_test, y_test) = mnist.load_data()
+
+    x_test = x_test.astype('float32') / 255.
+
+    grouped_images = {x: list() for x in range(10)}
+    for i in range(len(x_test)):
+        grouped_images[y_test[i]].append(x_test[i])
+
+    for n in range(1, 21):
+
+        file_path = os.path.join(
+            model_dir, f"v1_n_{n}"
+        )
+        model = models.load_model(file_path)
+
+        fig: plt.Figure = plt.figure(figsize=[6.4, 4.6], dpi=400)
+        outer = gridspec.GridSpec(1, 10, wspace=0.5)
+
+        for value in range(10):
+            inner = outer[value].subgridspec(10, 2)
+            for row in range(10):
+                left = fig.add_subplot(inner[row, 0])  # type: plt.Axes
+                right = fig.add_subplot(inner[row, 1])  # type: plt.Axes
+                left.imshow(grouped_images[value][row], cmap='Greys')
+                right.imshow(
+                    model.predict(
+                        np.expand_dims(grouped_images[value][row], 0)
+                    )[0], cmap='Greys'
+                )
+                left.grid(False)
+                right.grid(False)
+                left.set_xticklabels([])
+                left.set_yticklabels([])
+                right.set_xticklabels([])
+                right.set_yticklabels([])
+
+        fig.suptitle(f"Inputs / Outputs for {n} Channel(s), Autoencoder v1")
+        fig.show()
+        save_file = os.path.join(
+            fig_dir, 'input-output', f'{n}_channels.png'
+        )
+        fig.savefig(save_file)
+
+
+def pca_visualization():
+    (x, y), (_, _) = mnist.load_data()
+
+    x = x.astype('float32') / 255.
+    x = x.reshape(x.shape[0], (x.shape[1] * x.shape[2]))
+
+    pca = PCA(n_components=2)
+    x_r = pca.fit(x).transform(x)
+
+    colors = ['C' + str(x) for x in range(10)]
+    target_names = [str(x) for x in range(10)]
+
+    fig: plt.Figure = plt.figure(figsize=[6.4, 4.6], dpi=400)
+    ax: plt.Axes = fig.add_subplot()
+
+    for color, val, name in zip(colors, range(10), target_names):
+        plt.scatter(
+            x_r[y == val, 0], x_r[y == val, 1],
+            color=color, alpha=0.3, s=1, label=name
+        )
+
+    ax.set_title("2D PCA Analysis of training data")
+    leg = ax.legend(markerscale=5)
+    for lh in leg.legendHandles:
+        lh.set_alpha(1)
+
+    fig.show()
+
+    save_file = os.path.join(fig_dir, 'pca analysis.png')
     fig.savefig(save_file)
 
 
